477fb47eab2ba894b31edc832fe1839738a3ae0c
ImGabis <artediverso.lab@gmail.com>
2025-09-25 16:34:51 -0500

    Update main.jsx

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
 createRoot(document.getElementById("root")).render(<React.StrictMode><ThemeProvider><CartProvider><App/></CartProvider></ThemeProvider></React.StrictMode>);
 
